a4f5c19bc8ca4da751028d4bf791d3c9f8c846ea
Philip Macheier <philip.macheiner@edu.fh-joanneum.at>
2026-06-07 15:00:44 +0200

    -Bugfixing to enable Mysql connection in the cloud -Added health restpoint to check on healthiness

--- Backend/src/main/java/org/librime/libribackend/Security/SecurityConfig.java
+++ Backend/src/main/java/org/librime/libribackend/Security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/health").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/jobs/**").permitAll() // Allow worker updates
                         .anyRequest().authenticated()
                 )
